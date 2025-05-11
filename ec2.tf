3328ffa5216d3cf66c1b686fe19fe5e6ac7ecca1
goofy-56 <mohammed567abdul@gmail.com>
2025-05-11 19:52:21 +0530

    string update

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "loading" {
   instance_type = "t2.micro"
 subnet_id   = aws_subnet.crm-web-sn.id
 key_name = "almas.pem"
-vpc_security_group_ids = aws_security_group.crm-web-security.id
+vpc_security_group_ids = [aws_security_group.crm-web-security.id]
 user_data = file("setup.sh")
 }
 
