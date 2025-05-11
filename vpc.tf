49feb9ce82c142b0ae199aa01f1da215418b2723
goofy-56 <mohammed567abdul@gmail.com>
2025-05-11 11:03:56 +0530

    creat subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,28 @@ resource "aws_vpc" "crm-vpc" {
     Name = "crm-vpc"
   }
 }
+#subnet
+resource "aws_subnet" "crm-web-sn" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "crm-web-sn"
+  }
+}
+resource "aws_subnet" "crm-api-sn" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "crm-api-sn"
+  }
+}
+resource "aws_subnet" "crm-db-sn" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "crm-db-sn"
+  }
+}
