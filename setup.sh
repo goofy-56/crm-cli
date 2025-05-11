7b35aee2ee9f2b4fa5480f3825283d80fb0d9a23
goofy-56 <mohammed567abdul@gmail.com>
2025-05-11 20:32:13 +0530

    update

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 sudo apt update -y
 sudo apt install nginx -y
 sudo rm -rf /var/www/html/*
+#sudo git clone https://github.com/goofy-56/login-2421.git /var/www/html
 sudo git clone https://github.com/goofy-56/login-2421.git /var/www/html
